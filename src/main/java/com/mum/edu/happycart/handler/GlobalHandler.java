38226079602d5fcf00b7d9286d5c19adda806068
Natnael Welday <natnael.welday@gmail.com>
2015-07-14 17:34:08 -0500

    + Global Handler for objects that need to be available in memory and used across several controllers

--- src/main/java/com/mum/edu/happycart/handler/GlobalHandler.java
+++ src/main/java/com/mum/edu/happycart/handler/GlobalHandler.java
@@ -1,6 +1,10 @@
 package com.mum.edu.happycart.handler;
 
+import java.text.DateFormatSymbols;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -9,11 +13,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.mum.edu.happycart.domain.Category;
 import com.mum.edu.happycart.domain.Product;
+import com.mum.edu.happycart.enumerations.CardType;
+import com.mum.edu.happycart.service.AppSettingsService;
 import com.mum.edu.happycart.service.CategoryService;
 import com.mum.edu.happycart.service.ProductService;
 
 /**
  * Contains Exception Handler methods
+ * 
  * @author NatnaelW
  *
  */
@@ -22,18 +29,50 @@ public class GlobalHandler {
 
 	@Autowired
 	CategoryService categoryService;
-	
+
 	@Autowired
 	ProductService productService;
+	
+	@Autowired
+	AppSettingsService appSettingsService;
 
 	@ModelAttribute("categories")
-	public List<Category> intializeModel(Model model){
+	public List<Category> intializeModel(Model model) {
 		return categoryService.findAllCategories();
 	}
 
 	@ModelAttribute("products")
-	public List<Product> intializeModelProduct(Model model){
+	public List<Product> intializeModelProduct(Model model) {
 		return productService.findAll();
 	}
+
+	@ModelAttribute("creditCardTypes")
+	public List<String> initializeCreditCardType(Model model) {
+
+		List<String> creditCardTypes = new ArrayList<String>();
+		creditCardTypes.add((CardType.VISA).toString());
+		creditCardTypes.add((CardType.MASTER).toString());
+		return creditCardTypes;
+
+	}
+
+	@ModelAttribute("months")
+	public List<String> initMonths() {
+		List<String> months = new ArrayList<String>();
+		for (int i = 0; i < 12; i++) {
+			months.add(new DateFormatSymbols().getShortMonths()[i]);
+		}
+		return months;
+	}
 	
+	@ModelAttribute("cartSessionId")
+	public String getCartSessionId(HttpServletRequest request){
+		return request.getSession(true).getId();
+		}
+	
+	public void initIessages(Model model){
+		model.addAttribute("ErrorBalance",appSettingsService.appSettings("ErrorBalance").getParamValue().toString());
+		model.addAttribute("ErrorCard",appSettingsService.appSettings("ErrorCard").getParamValue().toString());
+	}
+
 }
